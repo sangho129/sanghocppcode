681bff9fdaea4a6c40dc9d072f6435f0a60b85d2
sangho129 <sangho129@icloud.com>
2022-12-16 20:37:17 +0900

    base code

---
+++ boj/2557.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
---
+++ bs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
