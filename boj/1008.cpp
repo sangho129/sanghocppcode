4a5039ceb393367e4cfd6b0803ac45194d57dce1
sangho129 <sangho129@icloud.com>
2022-12-16 20:44:29 +0900

    A / B

---
+++ boj/1008.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << A / B << endl;
+    return 0;
+}
