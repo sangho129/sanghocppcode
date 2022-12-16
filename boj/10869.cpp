fa769f2e9e85df4d61d675c861e1d4b7409c3f39
sangho129 <sangho129@icloud.com>
2022-12-16 20:51:55 +0900

    사칙연산

---
+++ boj/10869.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << endl
+         << A - B << endl
+         << A * B << endl
+         << A / B << endl
+         << A % B << endl;
+
+    return 0;
+}
