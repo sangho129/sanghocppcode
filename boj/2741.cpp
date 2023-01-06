900d38413c3e319b93e87a9c779b18e75d48c168
sangho129 <sangho129@icloud.com>
2023-01-06 17:19:48 +0900

    2741

---
+++ boj/2741.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+        cout << i << "\n";
+    return 0;
+}
