7b2fe55e6d06fcb4d8282d7cd1a29766df4e21cf
sangho129 <sangho129@icloud.com>
2023-01-06 17:47:40 +0900

    2742

---
+++ boj/2742.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    cin >> N;
+    for (int i = N; i > 0; i--)
+        cout << i << "\n";
+    return 0;
+}
